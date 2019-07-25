5f2f6c4587225b6544d2367d6f883900c22f4170
Frostiblue <46687289+Frostiblue@users.noreply.github.com>
2019-07-25 19:35:28 -0400

    commit 1

---
+++ Assets/Pictures/Scripts/BallControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+	float launchForce = 600;
+	float charge = 10;
+	public Rigidbody ballRB;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			LaunchBall();
+
+		}
+		if (Input.GetMouseButton(0))
+		{
+			charge += 10 * Time.deltaTime;
+		}
+	}
+	public void LaunchBall()
+	{
+
+	 ballRB.AddForce(new Vector3(0, launchForce, launchForce));
+		
+
+	}
+}
---
+++ Assets/Pictures/Scripts/BrickWall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickWall : MonoBehaviour
+{
+	public Vector3 topPos;
+	public Vector3 bottomPos;
+
+	public bool up;
+
+
+	// Start is called before the first frame update
+	void FixedUpdate()
+	{
+		if (up == false)
+		{
+			transform.position = Vector3.Lerp(transform.position, bottomPos, 7 * Time.deltaTime);
+		}
+		if (up == true)
+		{
+			transform.position = Vector3.Lerp(transform.position, topPos, 7 * Time.deltaTime);
+		}
+		if (transform.position == topPos)
+		{
+			up = false;
+		}
+		if (transform.position == bottomPos)
+		{
+			up = true;
+		}
+	}
+}
